3e245ea678601ea0993703e74692db4d109da488
jnu3d1 <jnu3d1@rambler.ru>
2022-08-27 14:25:37 +0600

    Этап 1 Изменения, которые включены в коммит: 	новый файл:    fixtures/auth.json 	новый файл:    fixtures/dump.json 	изменено:      python_12_exam_8_pavel_leonov/settings.py 	изменено:      python_12_exam_8_pavel_leonov/urls.py 	изменено:      webapp/admin.py 	новый файл:    webapp/migrations/0001_initial.py 	изменено:      webapp/models.py

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from webapp.models import Product, Review
+
+
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    list_display_links = ['name']
+    list_filter = ['name', 'category']
+    search_fields = ['category']
+    fields = ['name', 'category', 'description', 'image']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['product', 'author', 'rating']
+    list_display_links = ['product']
+    list_filter = ['product', 'rating']
+    search_fields = ['product', 'moderated']
+    fields = ['author', 'product', 'text', 'moderated', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Review, ReviewAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,55 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+
+categories = [
+    ('cat1', 'Категория 1'),
+    ('cat2', 'Категория 2'),
+    ('cat3', 'Категория 3'),
+    ('cat4', 'Категория 4'),
+    ('other', 'Разное')
+
+]
+
+stars = [
+    (1, '*'),
+    (2, '**'),
+    (3, '***'),
+    (4, '****'),
+    (5, '*****')
+]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование товара')
+    category = models.CharField(choices=categories, default='other', max_length=5, verbose_name='Категория товара')
+    description = models.TextField(blank=True, max_length=2000, verbose_name='Описание товара')
+    image = models.ImageField(blank=True, null=True, upload_to='images', verbose_name='Изображение товара')
+
+    def __str__(self):
+        return f'{self.id} {self.name} {self.category}'
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+
+class Review(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reviews', verbose_name='Автор')
+    product = models.ForeignKey('webapp.Product', on_delete=models.CASCADE, related_name='reviews',
+                                verbose_name='Товар')
+    text = models.TextField(max_length=3000, verbose_name='Отзыв о товаре')
+    rating = models.CharField(choices=stars, default=1, max_length=1, verbose_name='Оценка')
+    moderated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+
+    def __str__(self):
+        return f'{self.author} {self.product.name} {self.rating}'
+
+    class Meta:
+        db_table = 'reviews'
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
